663ae5173ce617b3dc1bfaaf0528fa679aac158c
Jose Moreno <josexmoreno1998@gmail.com>
2023-10-07 23:50:42 -0300

    overengeniering

--- src/app/Components/task-list/task-list.component.ts
+++ src/app/Components/task-list/task-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ListType } from '../../list-type';
 import { DataService } from '../../data.service';
-import { TaskItemComponent } from '../task-item/task-item.component';
 
 @Component({
   selector: 'app-task-list',
@@ -10,11 +9,8 @@ import { TaskItemComponent } from '../task-item/task-item.component';
       class="bg-white pt-8 pb-4 | lg:grid lg:grid-cols-3 lg:pt-20 lg:mx-auto lg:max-w-5xl lg:gap-x-8  "
     >
       <app-task-item
-        class="mb-6 py-4 px-6 mx-auto bg-soft-gray rounded-md w-[90%] text-dark-blue |  lg:mx-0 lg:py-0 lg:w-full lg:items-center lg:hover:col-span-2"
         *ngFor="let listItem of list; let i = index"
-        [class.lg:hover:col-start-2]="i === 2 && 5"
-        [class.lg:hover:row-start-1]="i === 2"
-        [class.lg:hover:row-start-2]="i === 5"
+        [class]="hoverIteraction(i + 1)"
         [item]="listItem"
         [index]="i"
         (taskAction)="taskStateChange($event)"
@@ -29,6 +25,21 @@ export class TaskListComponent {
     this.list = this.dataService.getAllData();
   }
 
+  hoverIteraction = (index: number) => {
+    let baseClass: string =
+      'mb-6 py-4 px-6 mx-auto bg-soft-gray rounded-md w-[90%] text-dark-blue |  lg:mx-0 lg:py-0 lg:w-full lg:items-center';
+    let lastItemRowClass: string = 'lg:hover:col-start-2 lg:hover:col-span-2';
+
+    let fixRowClass: number = Math.trunc((index + 3) / 3);
+    console.log(fixRowClass.toString());
+
+    if ((index + 1) % 3 === 0) {
+      return `${baseClass} ${lastItemRowClass}`;
+    } else {
+      return `${baseClass} lg:hover:col-span-2`;
+    }
+  };
+
   taskStateChange = (task: any) => {
     if (task.action === 'completed') {
       this.dataService.completedTask(task.id);
@@ -39,3 +50,5 @@ export class TaskListComponent {
     this.list = this.dataService.getAllData();
   };
 }
+// [class.lg:hover:col-start-2]="(i + 1) % 3 === 0" // here we use the remainder to assign the class only if i is dividable by 3 since every row will end with a third box in it
+// value + 3  = result / 3 = row
