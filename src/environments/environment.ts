d094dcd2d7dc35d663d8e42d3a6e9ced484d7d97
Jose <josexmoreno1998@gmail.com>
2024-01-24 17:59:58 -0300

    edit api url env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API_URL: 'https://todo-api-express-cyan.vercel.app',
+  API_URL: 'https://todo-api-express-cyan.vercel.app/',
   API_TEST: 'http://localhost:3050/'
 };
