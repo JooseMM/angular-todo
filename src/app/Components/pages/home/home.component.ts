cfb529ba53503402dd56e8fa4ea5d218cdc45527
Jose <josexmoreno1998@gmail.com>
2024-01-26 21:04:52 -0300

    mobile bug?

--- src/app/Components/pages/home/home.component.ts
+++ src/app/Components/pages/home/home.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  response = '';
+  constructor(private dataService: DataService){
+     this.dataService.getNotifications()
+     .subscribe((res)=> this.response = res);
+
+  }
+}
