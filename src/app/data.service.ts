3c2c5cbfecccbdebeaef716f3297f81532673232
Jose <josexmoreno1998@gmail.com>
2024-02-18 11:06:41 -0300

    change logout request to post

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -70,7 +70,7 @@ export class DataService {
       'Pragma': 'no-cache',
       'Expires': '0'
     });
-    this.http.get<UserLoggedIn>(`${environment.API_URL}logout`, { headers: headers, withCredentials: true})
+    this.http.post<UserLoggedIn>(`${environment.API_URL}logout`, { headers: headers, withCredentials: true})
       .subscribe({
         next: (response: UserLoggedIn) => {
           if(response.ok) {
